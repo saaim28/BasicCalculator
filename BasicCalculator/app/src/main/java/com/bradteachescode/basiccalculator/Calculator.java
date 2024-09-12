7d0914aa8c1faaf99edf98e40b4215df9b4338ad
saaim28 <67903095+saaim28@users.noreply.github.com>
2024-09-12 12:49:20 -0400

    Initial code for passing TDD test

---
+++ BasicCalculator/app/src/main/java/com/bradteachescode/basiccalculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.bradteachescode.basiccalculator;
+
+public class Calculator {
+    public double calculate(String expression) {
+        // Temporary implementation to make the test pass
+        return 6.9;
+    }
+}
